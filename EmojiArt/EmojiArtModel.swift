888698f44b03ccc485ffb476f030d70b1aa47ea9
Yuriy Lutsenko <lutsenko.yuriy93@gmail.com>
2023-01-15 00:52:04 +0100

    Add models

---
+++ EmojiArt/EmojiArtModel.swift
@@ -0,0 +1,39 @@
+//
+//  EmojiArtModel.swift
+//  EmojiArt
+//
+//  Created by Yuriy Lutsenko on 15.01.23.
+//
+
+import Foundation
+
+struct EmojiArtModel {
+    var background = Background.blank
+    var emojis = [Emoji]()
+    
+    struct Emoji: Identifiable, Hashable {
+        let text: String
+        var x: Int
+        var y: Int
+        var size: Int
+        let id: Int
+        
+        fileprivate init(text: String, x: Int, y: Int, size: Int, id: Int) {
+            self.text = text
+            self.x = x
+            self.y = y
+            self.size = size
+            self.id = id
+        }
+    }
+    
+    init() { }
+    
+    private var latestEmojiId = 0
+    
+    mutating func add(emoji text: String, at location: (x: Int, y: Int), size: Int) {
+        latestEmojiId += 1
+        emojis.append(Emoji(text: text, x: location.x, y: location.y, size: size, id: latestEmojiId))
+    }
+    
+}
