125fe0cb18f1254a7b3acf7b75e354f9de6df905
Yuriy Lutsenko <lutsenko.yuriy93@gmail.com>
2023-01-14 23:48:51 +0100

    Initial Commit

---
+++ EmojiArt/EmojiArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiArtApp.swift
+//  EmojiArt
+//
+//  Created by Yuriy Lutsenko on 14.01.23.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
