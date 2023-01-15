0e0979c5ddd863f36cc9afe08fc737f04b635fdf
Yuriy Lutsenko <lutsenko.yuriy93@gmail.com>
2023-01-15 11:43:57 +0100

    Add some UI elements

--- EmojiArt/EmojiArtDocumentView.swift
+++ EmojiArt/EmojiArtDocumentView.swift
@@ -11,12 +11,41 @@ struct EmojiArtDocumentView: View {
     @ObservedObject var document: EmojiArtDocument
     
     var body: some View {
-        
+        VStack(spacing: 0) {
+            documentBody
+            palette
+        }
+    }
+    
+    var documentBody: some View {
+        Color.yellow
+    }
+    
+    var palette: some View {
+        ScrollingEmojisView(emojis: testEmojis)
+    }
+    
+    let testEmojis = "👨👩👧👧"
+}
+
+struct ScrollingEmojisView: View {
+    
+    let emojis: String
+    
+    var body: some View {
+        ScrollView(.horizontal) {
+            HStack {
+                ForEach(emojis.map { String($0) }, id: \.self) { emoji in
+                    Text(emoji)
+                }
+            }
+        }
     }
 }
 
 struct EmojiArtDocumentView_Previews: PreviewProvider {
     static var previews: some View {
         EmojiArtDocumentView(document: EmojiArtDocument())
+            .previewInterfaceOrientation(.landscapeRight)
     }
 }
