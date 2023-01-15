5a249f30a966059af3e803e869149c2611641683
Yuriy Lutsenko <lutsenko.yuriy93@gmail.com>
2023-01-15 01:07:50 +0100

    Add view model

---
+++ EmojiArt/EmojiArtDocument.swift
@@ -0,0 +1,47 @@
+//
+//  EmojiArtDocument.swift
+//  EmojiArt
+//
+//  Created by Yuriy Lutsenko on 15.01.23.
+//
+
+import SwiftUI
+
+class EmojiArtDocument: ObservableObject {
+    
+    @Published private(set) var emojiArt: EmojiArtModel
+    
+    init() {
+        emojiArt = EmojiArtModel()
+    }
+    
+    var emojis: [EmojiArtModel.Emoji] {
+        emojiArt.emojis
+    }
+    
+    var background: EmojiArtModel.Background {
+        emojiArt.background
+    }
+    
+    func setBackground(_ background: EmojiArtModel.Background) {
+        emojiArt.background = background
+    }
+    
+    func add(emoji: String, at location: (x: Int, y: Int), size: CGFloat) {
+        emojiArt.add(emoji: emoji, at: location, size: Int(size))
+    }
+    
+    func move(emoji: EmojiArtModel.Emoji, by offset: CGSize) {
+        if let index = emojiArt.emojis.firstIndex(of: emoji) {
+            emojiArt.emojis[index].x += Int(offset.width)
+            emojiArt.emojis[index].y += Int(offset.height)
+        }
+    }
+    
+    func scale(emoji: EmojiArtModel.Emoji, by scale: CGFloat) {
+        if let index = emojiArt.emojis.firstIndex(of: emoji) {
+            emojiArt.emojis[index].size = Int((CGFloat(emojiArt.emojis[index].size) * scale).rounded(.toNearestOrAwayFromZero))
+        }
+    }
+    
+}
